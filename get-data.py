ebc2aaacbdb4610a8ba1d0daf90a0626fb561430
Rajan <rajansaini691@gmail.com>
2020-01-10 22:22:17 -0800

    Write webscraper

--- get-data.py
+++ get-data.py
@@ -1,9 +1,42 @@
 """
 Downloads the data used to train the model
 """
+from pandas import read_csv
+import pandas as pd
+import requests
+import json
 
-def get_acoustic_brainz():
+
+def get_acoustic_brainz(path):
     """
-    Downloads the data from the acoustic brainz dataset, which contains
+    Downloads data from the acoustic brainz dataset, which contains
     labelled audio files
+
+    Parameters:
+        path        Path to the folder to be populated with the data. The
+                    folder should contain a CSV with a list of musicbrainz IDs
+                    called dataset_annotations.csv
     """
+    # TODO Don't always assume path ends in a /
+    path_to_ids = path + "dataset_annotations.csv"
+
+    # Read the csv into a pandas dataframe
+    df = pd.read_csv(path_to_ids, delimiter=",", encoding="ascii")
+
+    # Download the ground truth data for each MB ID
+    for index, value in df['ID'].iteritems():
+        url = f"https://acousticbrainz.org/api/v1/{value}/low-level"
+        request = requests.get(url)
+        data = request.json()
+
+        # Get title
+        title = data['title']
+        print(title)
+        exit(1)
+
+        # Write json to disk
+
+        # Search for top result of title on yt and download
+
+if __name__ == "__main__":
+    get_acoustic_brainz("./data/acousticbrainz/")
